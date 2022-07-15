2e8579cb46f856c4cc3f3086f28482a510c513a5
anarakinson <anarakinson@gmail.com>
2022-07-15 18:54:31 +0300

    git versions lesson done

--- go-mod/go.mod
+++ go-mod/go.mod
@@ -2,7 +2,12 @@ module gopackages
 
 go 1.17
 
-require github.com/fatih/color v1.13.0
+require (
+	github.com/anarakinson/go_test_module v1.0.4
+	github.com/anarakinson/go_test_module/v2 v2.0.0
+	github.com/anarakinson/go_test_module/v3 v3.0.1
+	github.com/fatih/color v1.13.0
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.9 // indirect
--- go-mod/main.go
+++ go-mod/main.go
@@ -5,6 +5,9 @@ import (
     "gopackages/words"
     _ "gopackages/init"
     "github.com/fatih/color"
+    "github.com/anarakinson/go_test_module" // package name - <utils> - is used below, not repository name - <go_test_module>
+    utilsv2 "github.com/anarakinson/go_test_module/v2" // package from other github branch - v2
+    utilsv3 "github.com/anarakinson/go_test_module/v3" // package from other github branch - v3
 )
 
 func main() {
@@ -13,4 +16,25 @@ func main() {
         fmt.Println(words.Random())
     }
     color.Red("Red text")
+
+    fmt.Println("---")
+
+    var target = "three"
+    var slc = []string{"zero", "one", "two", "three", "four"}
+    fmt.Println("Searching for word:", target, "in slice:", slc)
+    res := utils.Contains(slc, target)
+    fmt.Println("Target is in slice:", res)
+
+    fmt.Println("---")
+
+    res = utilsv3.InSlice(slc, target)
+    fmt.Println("Target is in slice(v3):", res)
+
+    fmt.Println("---")
+
+    var targetI = 3
+    var slcI = []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+    fmt.Println("Searching for word:", targetI, "in slice:", slcI)
+    resI := utilsv2.InSliceInt(slcI, targetI)
+    fmt.Println("Target is in slice:", resI)
 }
